d425a94046f5af6bce3e030e4416070d2a4913ab
Anderson AUGUSMA <andersonaugusma@gmail.com>
2021-04-21 00:34:45 +0200

    derniere modification

--- websiteSRI20/clients/views.py
+++ websiteSRI20/clients/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 #from django.http import HttpResponse
-#from .models import Article
+from .models import Client
 # Create your views here.
 @login_required(login_url='acces')
 def list_clients(request):
-    return render(request,'clients/list_clients.html')
-    #article=Article.objects.all()
-    #return render(request,'index.html', {'article':article})
+    client=Client.objects.all()
+    return render(request,'clients/list_clients.html', {'client':client})
